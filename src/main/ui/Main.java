e1f803a4bbc4027013146ec0826457b152472530
fred62879 <zhuotingxie-fred@outlook.com>
2019-10-06 16:29:42 -0700

    "Deliv5.1 checkstyle checked!"

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -9,15 +9,6 @@ public class Main {
     public Main() throws IOException {
         Process p = new Process();
         p.run();
-        /*
-        int[] a = { 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1 };
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 10; j++) {
-                System.out.print(a[i * 10 + j] + ", ");
-            }
-            System.out.println();
-        }
-         */
     }
 
     public static void main(String[] args) throws IOException {
