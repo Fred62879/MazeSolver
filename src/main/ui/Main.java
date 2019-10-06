767ae3fe31c0839fe262974b7ce4aca9a4833a9c
fred62879 <zhuotingxie-fred@outlook.com>
2019-10-06 16:25:29 -0700

    "Deliv5.1 DFS test created!"

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -1,6 +1,7 @@
 package ui;
 
 import model.Process;
+
 import java.io.IOException;
 
 public class Main {
@@ -8,13 +9,15 @@ public class Main {
     public Main() throws IOException {
         Process p = new Process();
         p.run();
-        String res = "";
-        String a = "1 1 0 1 0 0 0 0 0 0 0 1 1 1 1 0 0 1 1 1 1 1 0 0 1 0 0 1 0 1 1 0 1 1 1 1 1 1 0 1 1 1 0 0 0 1 0 0 0 1 0 1 0 0 0 1 0 0 1 1 1 1 0 0 0 1 1 1 0 1 1 1 1 1 1 0 1 0 1 1 1 0 0 0 0 0 1 0 0 1 1 1 1 1 1 1 1 0 1 1";
-        for (int i = 0; i < a.length(); i += 2) {
-            res += a.substring(i, i + 1) + ", ";
+        /*
+        int[] a = { 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1 };
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                System.out.print(a[i * 10 + j] + ", ");
+            }
+            System.out.println();
         }
-        // System.out.println(res);
-
+         */
     }
 
     public static void main(String[] args) throws IOException {
--- src/test/ui/SolverDFSTest.java
+++ src/test/ui/SolverDFSTest.java
@@ -1,36 +1,35 @@
 package ui;
 
-import model.Maze;
 import model.SolverDFS;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 
 public class SolverDFSTest {
 
-    Maze mz1, mz2, mz3, mz4;
-    ArrayList<Integer> ip1, ip2, ip3, ip4;
-    SolverDFS sdfs;
+    int[][] mtrx1, mtrx2, mtrx3, mtrx4;
+    SolverDFS sdfs1, sdfs2, sdfs3, sdfs4;
 
     @BeforeEach
     public void setup() {
-        ip1 = new ArrayList<>(Arrays.asList(1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1));
-        ip2 = new ArrayList<>(Arrays.asList(1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1));
-        System.out.println(ip2.size());
-        ip3 = new ArrayList<>(Arrays.asList(1, 0, 0, 0));
-        ip4 = new ArrayList<>(Arrays.asList(1, 1, 0, 1, 0, 1, 0, 1, 1));
-
-        mz1 = new Maze(ip1, 6, 7);
-        mz2 = new Maze(ip2, 10, 10);
-        mz3 = new Maze(ip3, 2, 2);
-        mz4 = new Maze(ip4, 3, 3);
+        mtrx1 = new int[][] { {1, 0, 0, 1, 1, 1, 1}, {1, 1, 1, 1, 0, 1, 1}, {1, 1, 0, 0, 1, 0, 0}, {0, 1, 0, 1, 1, 0, 0}, {0, 1, 0, 1, 0, 0, 0}, {0, 1, 1, 1, 1, 1, 1}};
+        mtrx2 = new int[][] { {1, 1, 0, 1, 0, 0, 0, 0, 0, 0},{0, 1, 1, 1, 1, 0, 0, 1, 1, 1},{1, 1, 0, 0, 1, 0, 0, 1, 0, 1},{1, 0, 1, 1, 1, 1, 1, 1, 0, 1},{1, 1, 0, 0, 0, 1, 0, 0, 0, 1},{0, 1, 0, 0, 0, 1, 0, 0, 1, 1},{1, 1, 0, 0, 0, 1, 1, 1, 0, 1},{1, 1, 1, 1, 1, 0, 1, 0, 1, 1},{1, 0, 0, 0, 0, 0, 1, 0, 0, 1},{1, 1, 1, 1, 1, 1, 1, 0, 1, 1}};
+        mtrx3 = new int[][] { {1, 0}, {0, 0} };
+        mtrx4 = new int[][] { {1, 1, 0}, {1, 0, 1}, {0, 1, 1} };
+        sdfs1 = new SolverDFS(mtrx1);
+        sdfs2 = new SolverDFS(mtrx2);
+        sdfs3 = new SolverDFS(mtrx3);
+        sdfs4 = new SolverDFS(mtrx4);
     }
 
     @Test
     public void testSolve() {
-        assertTrue()
+        assertTrue(sdfs1.solve(0, 0));
+        assertTrue(sdfs2.solve(0, 0));
+        assertFalse(sdfs3.solve(0, 0));
+        assertFalse(sdfs4.solve(0, 0));
     }
-
 }
