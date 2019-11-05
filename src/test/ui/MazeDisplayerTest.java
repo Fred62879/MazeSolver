896a0982ab39a169ea8f25f291a851798f17e985
fred62879 <zhuotingxie-fred@outlook.com>
2019-11-05 11:12:43 -0800

    "Deliv9 finalized"

---
+++ src/test/ui/MazeDisplayerTest.java
@@ -0,0 +1,46 @@
+package ui;
+
+import model.MazeDisplayer;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MazeDisplayerTest {
+
+    MazeDisplayer md;
+    List<Integer> path;
+
+    @BeforeEach
+    public void setup() {
+        int[][] matrix = {{1,0,1,1},{1,0,0,0}};
+        md = new MazeDisplayer(matrix);
+        path = new ArrayList<Integer>();
+        path.add(0);
+        path.add(1);
+        path.add(5);
+        path.add(6);
+        path.add(7);
+    }
+
+    @Test
+    public void testSetPath() {
+        md.setPath(path);
+        assertEquals(0, md.getPath(0));
+        assertEquals(1, md.getPath(1));
+        assertEquals(5, md.getPath(2));
+        assertEquals(6, md.getPath(3));
+        assertEquals(7, md.getPath(4));
+    }
+
+    @Test
+    public void testDisplayPath() {
+        assertFalse(md.displayPath());
+        md.setPath(path);
+        assertTrue(md.displayPath());
+    }
+
+}
